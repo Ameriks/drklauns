dfbe94af1c0a2519b62c615053189138f9e07afc
Agris Ameriks <github@pd.lv>
2016-10-26 16:43:01 +0300

    Bugfix. number of procedures isn't calculated.

--- drklauns/timetable/tasks.py
+++ drklauns/timetable/tasks.py
@@ -12,10 +12,12 @@ def recalculate_summary(work_id: int=None, work: Work=None):
     dt = datetime.date(work.start.year, work.start.month, 1)
     summary, created = Summary.objects.get_or_create(employee=work.employee, date=dt, defaults={'rate': work.employee.contract_rate})
 
-    totals = Work.objects.filter(start__year=work.start.year, start__month= work.start.month, employee=work.employee)\
-        .aggregate(hours_worked=Sum('hours_worked'), number_of_contacts=Sum('number_of_contacts'), number_of_procedures=Sum('number_of_procedures'))
+    totals = Work.objects.filter(start__year=work.start.year, start__month= work.start.month, employee=work.employee).aggregate(hours_worked=Sum('hours_worked'), number_of_contacts=Sum('number_of_contacts'), number_of_procedures=Sum('number_of_procedures'))
 
     summary.hours_worked = totals.get('hours_worked')
-    summary.number_of_contacts = totals.get('number_of_contacts')
-    summary.number_of_procedures = totals.get('number_of_procedures')
+    summary.total_contacts = totals.get('number_of_contacts')
+    summary.total_procedures = totals.get('number_of_procedures')
+
+    summary.income = summary.rate * summary.hours_worked
+
     summary.save()
